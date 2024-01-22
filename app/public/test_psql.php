1434626c4622d8b5fbae8989211a7ecfdc6b53b5
Pleskan <pleskan87@gmail.com>
2024-01-22 21:53:47 +0000

    Completely change project layout

--- app/public/test_psql.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $connection = pg_connect ("host=postgresql dbname=mydb user=myuser password=my_password");
-    if($connection) {
-	           echo 'connected';
-		       } else {
-			        echo 'there has been an error connecting';
-				           } 
-?>
-
